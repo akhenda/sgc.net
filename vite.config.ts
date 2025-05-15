bc54ea145343aef5c0f6815c22d94f003deee770
akhenda <akhenda@gmail.com>
2025-05-15 23:19:34 +0300

    build(repo): shadcn init

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,18 @@
 
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig, UserConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   test: {
     /* use global to avoid globals imports (describe, test, expect): */
-    // globals: true,
+    globals: true,
     coverage: { enabled: true, provider: 'v8', reporter: ['text-summary'] },
     reporters: ['default'],
     typecheck: {
       enabled: false, // Slows down test server. Still experimental
     },
   },
-});
+} as UserConfig);
